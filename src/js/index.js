3489c6a48d3d94a7060d3cd06d68b9e56a3f9137
Jarpiino <jarno.ojasaar@gmail.com>
2021-04-08 01:21:32 +0300

    commit 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import '../styles/main.scss'
+import '../styles/main.scss';
+import { gsap } from "gsap";
 
 const menuBtn = document.querySelector('#menu-btn');
 const navMenu = document.querySelector('#navmenu');
@@ -23,3 +24,18 @@ navLink.forEach(navLink => navLink.addEventListener("click", () => {
     navItem2.classList.toggle("open");
     navItem1.classList.toggle("open");
 }));
+
+const hero = document.querySelector(".home-content");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const hamburger = document.querySelector("#menu-btn");
+const headline = document.querySelector(".headline");
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, {height:'0%'},{duration: 1, height:'70%', ease: Power2.easeInOut})
+.fromTo(hero, {width: '100%'}, {duration: 1.2, width: '80%', ease: Power2.easeInOut})
+.fromTo(slider, {x: '-100%'}, {duration: 1.2, x: '0%', ease: Power2.easeInOut}, '-=1.2')
+.fromTo(logo, {opacity: 0, x:30}, {duration: 0.5, opacity: 1, x:0}, '-=0.5')
+.fromTo(hamburger, {opacity: 0, x:30}, {duration: 0.5, opacity: 1, x:0}, '-=0.7')
+.fromTo(headline, {opacity: 0}, {duration: 0.5, opacity: 1}, '-=0.5');
